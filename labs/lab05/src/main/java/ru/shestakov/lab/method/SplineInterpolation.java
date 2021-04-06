7bc3514384bfc3c28584aa244e817d50e0687f55
a18735215 <avshestakov@sberbank.ru>
2021-04-07 00:45:00 +0300

    no message

--- labs/lab05/src/main/java/ru/shestakov/lab/method/SplineInterpolation.java
+++ labs/lab05/src/main/java/ru/shestakov/lab/method/SplineInterpolation.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class SplineInterpolation {
     private List<Node> interpolationNodes;
+    private int numberOfIntervals;
     private double[] aValues;
     private double[] bValues;
     private double[] cValues;
@@ -13,21 +14,46 @@ public class SplineInterpolation {
 
     public SplineInterpolation(List<Node> interpolationNodes) {
         this.interpolationNodes = interpolationNodes;
+        init();
+        calculateCoefficients();
     }
 
     public double calculate(double xValue) {
         return 0d;
     }
 
-    public void initSplines() {
-        init();
+    private void calculateCoefficients() {
+        cValues = new TridiagonalMatrixAlgorithm(prepareMatrix()).calculate();
+    }
+
+    private double[][] prepareMatrix() {
+        double[][] result = new double[interpolationNodes.size()][interpolationNodes.size()+1];
+        for (int i=1; i<interpolationNodes.size(); i++) {
+            double hCurr = interpolationNodes.get(i).getX() - interpolationNodes.get(i-1).getX();
+            double hNext = interpolationNodes.get(i+1).getX() - interpolationNodes.get(i).getX();
+            double a = hCurr;
+            double b = hNext;
+            double c = 2 * (hCurr + hNext);
+            double yCurr = interpolationNodes.get(i).getY();
+            double yNext = interpolationNodes.get(i+1).getY();
+            double yPrev = interpolationNodes.get(i-1).getY();
+            double d = 3 * ((yNext - yCurr) / hNext - (yCurr - yPrev) / hCurr);
+
+            result[i][i] = a;
+            result[i-1][i-1] = c;
+            result[i-1][i] = b;
+            result[i-1][result.length-1] = d;
+        }
+        return result;
     }
 
     private void init() {
-        int size = interpolationNodes.size()-1;
-        aValues = new double[size];
-        bValues = new double[size];
-        cValues = new double[size];
-        dValues = new double[size];
+        // Количество интервалов (сплайнов)
+        numberOfIntervals = interpolationNodes.size()-1;
+        // Коэффициенты a, b, c, d
+        aValues = new double[numberOfIntervals];
+        bValues = new double[numberOfIntervals];
+        cValues = new double[numberOfIntervals];
+        dValues = new double[numberOfIntervals];
     }
 }
